4c9c9858af2bb5339b95b13ae791bbe8282d6f6c
Jim Filippou <jimfilippou8@gmail.com>
2020-08-03 18:42:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+(function (window) {
+    'use strict';
+
+    function kostasotrelos0() {
+        var _trelos = {};
+        trelos.introduce = function (opts = {}) {
+            const message = 'Καλησπέρα friends! ορίστε!';
+            opts.needsAttention ? alert(message) : console.log(message);
+            return console.log('');
+        };
+        trelos.sayKalispera = function () {
+            var audio = new Audio('friends.mp3');
+            audio.play();
+        };
+        return _trelos;
+    }
+
+    if (typeof window.kostasotrelos0 === 'undefined') {
+        window.globalTrelos = kostasotrelos0();
+    }
+})(window);
+
+console.log(kostasotrelos0);
---
+++ readme.md
@@ -0,0 +1,8 @@
+<h1 align="center">
+  <br>
+  <a href="https://webtorrent.io"><img src="https://user-images.githubusercontent.com/6877046/89200562-e15eb300-d5b8-11ea-9378-fac22059baa0.png" alt="kostas" width="200"></a>
+  <br>
+  oriste.js
+  <br>
+  <br>
+</h1>
