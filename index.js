b051fcbec1e898fd5ad7f9d32c5f80c8b26a9361
George Kanellos <geokane@hotmail.com>
2020-10-21 19:03:56 +0300

    add kalispera from anastasia giousef

--- index.js
+++ index.js
@@ -24,5 +24,20 @@
     if (typeof window.kostasotrelos0 === 'undefined') {
         window.kostasotrelos0 = kostasotrelos0();
         console.log('[!] Kostasotrelos0 was loaded!');
-    }
+	}
+	
+	function anastasia(){
+		let anastasia = document.createElement('DIV');
+		anastasia.style.display = "none";
+		anastasia.innerHTML = `<h3>Bonus Kalispera!</h3><iframe width="560" height="315" src="https://www.youtube.com/embed/_k32P-M0AsY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`;
+		document.querySelector('body').appendChild(anastasia);
+		return function() {
+			anastasia.style.display = "block";
+		}
+	}
+
+	if (typeof window.anastasia === 'undefined') {
+        window.anastasia = anastasia();
+        console.log('[!] Anastasia was loaded!');
+	}
 })(window);
